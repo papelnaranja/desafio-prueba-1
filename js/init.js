1d2ed7b7f32d0e13814a3be50d96090d8633ca32
Vanessa Frenkel <vanefrenkel@gmail.com>
2018-11-14 21:31:02 -0300

    css y cambios

--- js/init.js
+++ js/init.js
@@ -2,7 +2,14 @@ $('#slides').superslides({
     play: 5000,
     //animation_speed: 500,
 });
-$('.slider-wrap').slick();
+$('.slider-wrap').slick({
+      dots: true,
+          infinite: true,
+          speed: 500,
+          fade: false,
+          cssEase: 'linear'
+
+});
 // $('.carousel').carousel({
 //     interval: 2000
 // })
